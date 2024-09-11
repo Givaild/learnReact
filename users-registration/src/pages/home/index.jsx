e65936c610c8dec90384e866d73dc4b5b539fd02
Givaildo de Andrade <65353339+Givaild@users.noreply.github.com>
2024-09-11 14:45:04 +0000

    fix: create a mock users

--- users-registration/src/pages/home/index.jsx
+++ users-registration/src/pages/home/index.jsx
@@ -1,6 +1,20 @@
 import "./style.css";
 
 function Home() {
+  const users = [
+    {
+      id: "14f5af2a1f5a4saa",
+      name: "Gustavo",
+      age: 9,
+      email: "gu@gmail.com",
+    },
+    {
+      id: "15145as1f1as514a59",
+      name: "Herinque",
+      age: 9,
+      email: "rick@gmail.com",
+    },
+  ];
   return (
     <div>
       <div className="container">
