0f202a3a4e601e5fe623ebe77f4137abff7a9495
Givaildo de Andrade <65353339+Givaild@users.noreply.github.com>
2024-09-14 14:53:30 +0000

    fix: css for card

--- users-registration/src/pages/home/index.jsx
+++ users-registration/src/pages/home/index.jsx
@@ -35,9 +35,15 @@ function Home() {
         {users.map((user) => (
           <div key={user.id} className="card">
             <div>
-              <p>Nome:{user.name}</p>
-              <p>Idade:{user.age}</p>
-              <p>Email:{user.email}</p>
+              <p>
+                Nome:<span>{user.name}</span>
+              </p>
+              <p>
+                Idade:<span>{user.age}</span>
+              </p>
+              <p>
+                Email:<span>{user.email}</span>
+              </p>
             </div>
             <button>
               <img src={Trash} />
