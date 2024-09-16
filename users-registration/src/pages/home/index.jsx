58cd0a9a626c4bdba2d16775ba1e6afae8685736
Givaildo de Andrade <65353339+Givaild@users.noreply.github.com>
2024-09-16 23:55:15 +0000

    feat: install and first config axios

--- users-registration/src/pages/home/index.jsx
+++ users-registration/src/pages/home/index.jsx
@@ -1,27 +1,12 @@
 import "./style.css";
 import Trash from "../../assets/trash-2.svg";
+import api from "../../services/api";
 
 function Home() {
-  const users = [
-    {
-      id: "14f5af2a1f5a4saa",
-      name: "Gustavo",
-      age: 9,
-      email: "gu@email.com",
-    },
-    {
-      id: "15145as1f1as514a59",
-      name: "Herinque",
-      age: 9,
-      email: "rick@email.com",
-    },
-    {
-      id: "1a2sa1f21af5",
-      name: "Gil",
-      age: 38,
-      email: "Gil@email.com",
-    },
-  ];
+  let users = [];
+  async function getUsers() {
+    users = await api.get("/users");
+  }
   return (
     <div>
       <div className="container">
